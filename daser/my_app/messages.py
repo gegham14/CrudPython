314467700f23a38137c1c2e60a34cfc635f1843e
gegham14 <157041855+gegham14@users.noreply.github.com>
2024-07-27 23:57:26 +0400

    update

---
+++ daser/my_app/messages.py
@@ -0,0 +1,2 @@
+add_success_message = 'Course created successfully.'
+add_error_message = 'Error creating course. Please check the form and try again.'
--- daser/my_app/views.py
+++ daser/my_app/views.py
@@ -2,11 +2,13 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Course
 from .forms import CourseForm
+from django.contrib import messages
+from .messages import add_success_message, add_error_message
 
 
 class CourseListView(ListView):
     model = Course
-    template_name = '.html'
+    template_name = 'my_app/course_list.html'
     context_object_name = 'courses'
 
 
@@ -24,6 +26,13 @@ class CourseCreateView(CreateView):
     template_name = 'course_form.html'
     success_url = reverse_lazy('course_list')
 
+    def form_valid(self, form):
+        messages.success(self.request, add_success_message)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, add_error_message)
+        return super().form_invalid(form)
 
 class CourseUpdateView(UpdateView):
     model = Course
@@ -33,10 +42,25 @@ class CourseUpdateView(UpdateView):
     slug_url_kwarg = 'slug'
     slug_field = 'slug'
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Course updated successfully.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Error updating course. Please check the form and try again.')
+        return super().form_invalid(form)
+
 
 class CourseDeleteView(DeleteView):
     model = Course
-    template_name = 'course_confirm_delete.html'
+    template_name = 'my_app/course_confirm_delete.html'
     success_url = reverse_lazy('course_list')
     slug_url_kwarg = 'slug'
     slug_field = 'slug'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, 'Course deleted successfully.')
+        return super().delete(request, *args, **kwargs)
+
+
+
